566d5fc0c88e21ab3c5bcebeca0d7d2019063b69
MelaqueMunim <144835047+MelaqueMunim@users.noreply.github.com>
2025-10-26 22:19:39 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//  Tailwind CSS config - tells Tailwind which files to scan for utility classes
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
